59641dc9957035e0ec49c219caf49524ebc31761
gachirigc <39502857+gachirigc@users.noreply.github.com>
2018-06-14 15:16:05 -0500

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","contactw_ecommer","061695","contactw_ecommerce");
+$con = mysqli_connect("localhost","mysqldbuser@amazonebay-mysqldbserver","Universidad2018","contactw_ecommerce");
 if(mysqli_connect_errno()){
     echo "The Connection was not established: " . mysqli_connect_error();
 }
